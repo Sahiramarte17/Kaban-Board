37f655dbbd8aff42d5b070f578d8374a5cce3e51
Sahiramarte17 <sahiramarte17@gmail.com>
2024-10-16 16:06:21 -0400

    updating login

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -42,7 +42,9 @@ class AuthService {
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/kanban');
   }
 
   logout() {
