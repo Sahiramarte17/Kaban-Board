083ed384c2e4e76de9a936d3bac2a4b5c7d8ae34
Sahiramarte17 <sahiramarte17@gmail.com>
2024-10-16 17:09:33 -0400

    Updating colors

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 import auth from '../utils/auth';
 
 const Navbar = () => {
-  const [ loginCheck, setLoginCheck ] = useState(false);
+  const [loginCheck, setLoginCheck] = useState(false);
 
   const checkLogin = () => {
-    if(auth.loggedIn()) {
+    if (auth.loggedIn()) {
       setLoginCheck(true);
     }
   };
@@ -14,7 +14,7 @@ const Navbar = () => {
   useEffect(() => {
     console.log(loginCheck);
     checkLogin();
-  }, [loginCheck])
+  }, [loginCheck]);
 
   return (
     <div className='nav'>
@@ -22,24 +22,32 @@ const Navbar = () => {
         <Link to='/'>Krazy Kanban Board</Link>
       </div>
       <ul>
-      {
-        !loginCheck ? (
-          <li className='nav-item'>
-            <button type='button'>
-              <Link to='/login'>Login</Link>
-            </button>
-          </li>
-        ) : (
-          <li className='nav-item'>
-            <button type='button' onClick={() => {
-              auth.logout();
-            }}>Logout</button>
-          </li>
-        )
-      }
+        {
+          !loginCheck ? (
+            <li className='nav-item'>
+              <button type='button'>
+                <Link to='/login'>Login</Link>
+              </button>
+            </li>
+          ) : (
+            <>
+              <li className='nav-item'>
+                <Link to='/new-ticket'>
+                  <button type='button'>New Ticket</button>
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <button type='button' onClick={() => {
+                  auth.logout();
+                }}>Logout</button>
+              </li>
+            </>
+          )
+        }
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
+
