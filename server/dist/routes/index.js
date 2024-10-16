ddbe2cad85772ec970a8e63ade868c6bb95ca471
Sahiramarte17 <sahiramarte17@gmail.com>
2024-10-16 16:38:10 -0400

    Fixing login button color

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
+import { authenticateToken } from '../middleware/auth.js';
 const router = Router();
+// Public auth routes
 router.use('/auth', authRoutes);
-// TODO: Add authentication to the API routes
-router.use('/api', apiRoutes);
+// Add authentication to the API routes
+router.use('/api', authenticateToken, apiRoutes); // Now all /api routes will require authentication
 export default router;
+// TODO: Add authentication to the API routes
