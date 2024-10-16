4f1ae8afbc6277567a27f11f3bd4c4399756f599
Sahiramarte17 <sahiramarte17@gmail.com>
2024-10-16 16:16:56 -0400

    updating interface

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,8 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 interface JwtPayload {
   username: string;
+  id: string;
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
